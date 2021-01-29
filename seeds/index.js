d3cc3a06624d35d8abffdbbc6def2e8b116eba19
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2021-01-29 11:20:02 -0600

    created variable seedDB -> added 50 cities to DB

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const Campground = require('./models/Campground');
+const cities = require('./cities');
+const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true, 
@@ -12,3 +13,16 @@ db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', () => {
     console.log('Database connected');
 });
+
+const seedDB = async() => {
+    await Campground.deleteMany({});
+    for(let i=0; i < 50; i++){
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB();
