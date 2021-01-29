1babde60e684943c5c060424f9f6f89bbe6182f5
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2021-01-29 11:39:56 -0600

    set up seed page

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -14,15 +15,21 @@ db.once('open', () => {
     console.log('Database connected');
 });
 
+const sample = array => array[Math.floor(Math.random() * array.length)]; //function to give a random element out of an array
+
+
 const seedDB = async() => {
-    await Campground.deleteMany({});
-    for(let i=0; i < 50; i++){
+    await Campground.deleteMany({}); //start by deleting everything from the DB
+    for(let i=0; i < 50; i++){   //get 50 cities randomly from cities.js
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`, //location is determined by random1000
+            title: `${sample(descriptors)} ${sample(places)}` //title is determined by our function 'sample', by plugging in descriptors and places
         })
         await camp.save();
     }
 }
 
-seedDB();
+seedDB().then(() => {
+    mongoose.connection.close();
+})
