eafcbe0f4644d1423a0e283dd4c6c5f0865fa0a8
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2021-02-10 12:39:41 -0600

    added images to show page

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seedDB = async() => {
     await Campground.deleteMany({}); //start by deleting everything from the DB
     for(let i=0; i < 50; i++){   //get 50 cities randomly from cities.js
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`, //location is determined by random1000
-            title: `${sample(descriptors)} ${sample(places)}` //title is determined by our function 'sample', by plugging in descriptors and places
+            title: `${sample(descriptors)} ${sample(places)}`, //title is determined by our function 'sample', by plugging in descriptors and places
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente accusantium tempore nulla sint atque eum itaque cumque, saepe sed doloribus placeat, eos nesciunt ut dolorem deleniti excepturi dolores, magni ea?',
+            price
+            
         })
         await camp.save();
     }
