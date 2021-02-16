e7de296add96bf16c6c232609bd9a6f774ae0a19
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2021-02-16 17:45:25 -0600

    Added form validations

--- app.js
+++ app.js
@@ -39,10 +39,14 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', async (req, res, next) => {
+    try{
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
+    } catch (e) {
+        next(e);
+    }
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
@@ -66,6 +70,10 @@ app.delete('/campgrounds/:id', async (req, res) => {
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 })
+
+app.use((err, req, res, next) => {
+    res.send("Oh boy, something went wrong!");
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
