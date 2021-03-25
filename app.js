34b91cce93d00933d3f0f4d36f789116619d80d9
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2021-03-25 09:42:13 -0500

    added returntoUrl when loggin in

--- app.js
+++ app.js
@@ -66,6 +66,7 @@ passport.serializeUser(User.serializeUser());  //how to store a user in a sessio
 passport.deserializeUser(User.deserializeUser());  //how to 'unstore' a user from a session
 
 app.use((req, res, next) => {
+    console.log(req.session);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
