7ecf1f173ea60e532e8d43bacce6317cd10289f6
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2021-03-28 22:56:06 -0500

    installed dotenv

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -12,8 +12,8 @@ const Campground = require('../models/campground');
 router.route('/')
     .get(catchAsync(campgrounds.index))
    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-    .post(upload.single('image'), (req, res) => {
-        res.send(req.body, req.file);
+    .post(upload.array('image'), (req, res) => {
+        res.send(req.body, req.files);
     })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
